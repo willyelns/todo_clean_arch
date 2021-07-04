dd39322c4d2ae44617bd250f860282602578fcbb
Will Xavier <willyelnsg.x@gmail.com>
2021-07-04 00:14:15 -0300

    refactor: separes the main from app

---
+++ lib/app.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+import 'home_page.dart';
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'To do list',
+      theme: ThemeData(
+        primarySwatch: Colors.purple,
+      ),
+      home: HomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
--- lib/commons/errors/exceptions/server_exception.dart
+++ lib/commons/errors/exceptions/server_exception.dart
@@ -1,5 +1,13 @@
-class ServerException implements Exception {
+import 'package:equatable/equatable.dart';
+
+class ServerException extends Equatable implements Exception {
   const ServerException({this.message});
 
   final String? message;
+
+  @override
+  bool? get stringify => true;
+
+  @override
+  List<Object?> get props => [message];
 }
